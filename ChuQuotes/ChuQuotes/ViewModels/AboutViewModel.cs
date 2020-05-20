6fd109106bc982b645bfd84cfaf2bdfdc4fe94ed
mturchik <mturchik@my.wctc.edu>
2020-05-19 22:43:38 -0500

    Strength Training Accelerometer!

--- ChuQuotes/ChuQuotes/ViewModels/AboutViewModel.cs
+++ ChuQuotes/ChuQuotes/ViewModels/AboutViewModel.cs
@@ -9,6 +9,7 @@ namespace ChuQuotes.ViewModels
     {
         public  ICommand OpenWebCommand       { get; }
         public  ICommand TrainStrengthCommand { get; }
+        private AccelerometerData Reading { get; set; }
         private double   strength  = 1;
         private double   willpower = 0;
 
@@ -26,6 +27,17 @@ namespace ChuQuotes.ViewModels
 
         public AboutViewModel()
         {
+            try
+            {
+                if(!Accelerometer.IsMonitoring)
+                    Accelerometer.Start(SensorSpeed.UI);
+                Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+
             Title          = "About ChuQuotes";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://api.chucknorris.io/"));
             TrainStrengthCommand = new Command(() =>
@@ -45,5 +57,25 @@ namespace ChuQuotes.ViewModels
                 }
             });
         }
+
+        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            if(!e.Reading.Acceleration.Equals(Reading.Acceleration))
+                switch (Strength)
+                {
+                    case 1:
+                        Strength++;
+                        break;
+                    case double.PositiveInfinity:
+                        Willpower += Willpower is 0 ? 1 : Willpower;
+                        Strength  =  Willpower;
+                        break;
+                    default:
+                        Strength = Math.Pow(Strength, 2);
+                        break;
+                }
+            Reading = e.Reading;
+        }
+
     }
 }
